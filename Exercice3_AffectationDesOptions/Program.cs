9fe1c31edda126f03a2f0bb2a97e74c68b7cb263
Hanane Aït-Lhaj <92375851+HN-Aitlhaj@users.noreply.github.com>
2021-10-18 21:33:41 +0100

    Add project files.

---
+++ Exercice3_AffectationDesOptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercice_affectation_des_options_de_la_filière_informatique_aux_étudiants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Exercice: affectation des options de la filière informatique aux étudiants
+
+/*1 - Modéliser et implémenter les associations suivantes:
+
+-Un étudiant doit obligatoirement candidater pour les trois options de la filière informatiques avec l'ordre de préférence.
+
+-A un étudiant est attribué une seule option selon l'ordre de mérite (note moyenne de la première année) et l'ordre de préférence qu'il a exprimer.
+
+2 - Implémenter l'algorithme d'attribution des options aux étudiants (Version simple de l'algorithme de Gale et Shapley).
+
+
+Livrables:
+
+Lien cliquable vers votre repository GitHub content une image du diagramme de classes UML, les classes résultantes C# ainsi que l'implémentation de l'algorithme d'attribution des option.*/
